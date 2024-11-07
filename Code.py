1df5bdd00a15484622e26608f1431a30a4a904e6
unknown <bindu29-garaga>
2024-11-07 21:04:38 +0530

    first commit

---
+++ Code.py
@@ -0,0 +1,69 @@
+from scipy.spatial import distance
+from imutils import face_utils
+import imutils
+import dlib
+import cv2
+from pygame import mixer
+
+mixer.init()
+mixer.music.load("bleep-41488.mp3")
+
+def eye_aspect_ratio(eye):
+    A = distance.euclidean(eye[1], eye[5])
+    B = distance.euclidean(eye[2], eye[4])
+    C = distance.euclidean(eye[0], eye[3])
+    ear = (A + B) / (2.0 * C)
+    return ear
+
+thresh = 0.25
+frame_check = 20
+detect = dlib.get_frontal_face_detector()
+predict = dlib.shape_predictor(r'C:\Users\Dell\Documents\Drowsiness_Detection-final\Drowsiness_Detection-final\shape_predictor_68_face_landmarks.dat')
+
+(lStart, lEnd) = face_utils.FACIAL_LANDMARKS_68_IDXS["left_eye"]
+(rStart, rEnd) = face_utils.FACIAL_LANDMARKS_68_IDXS["right_eye"]
+cap = cv2.VideoCapture(0)
+flag = 0
+alarm_status = False  # Flag to track if alarm has been triggered
+
+while True:
+    ret, frame = cap.read()
+    frame = imutils.resize(frame, width=450)
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    subjects = detect(gray, 0)
+    
+    for subject in subjects:
+        shape = predict(gray, subject)
+        shape = face_utils.shape_to_np(shape)
+        leftEye = shape[lStart:lEnd]
+        rightEye = shape[rStart:rEnd]
+        leftEAR = eye_aspect_ratio(leftEye)
+        rightEAR = eye_aspect_ratio(rightEye)
+        ear = (leftEAR + rightEAR) / 2.0
+        leftEyeHull = cv2.convexHull(leftEye)
+        rightEyeHull = cv2.convexHull(rightEye)
+        cv2.drawContours(frame, [leftEyeHull], -1, (0, 255, 0), 1)
+        cv2.drawContours(frame, [rightEyeHull], -1, (0, 255, 0), 1)
+        
+        if ear < thresh:
+            flag += 1
+            if flag >= frame_check:
+                if not alarm_status:  # Play alert sound only once
+                    print("Drowsy")
+                    mixer.music.play()
+                    alarm_status = True  # Set alarm status to True
+                cv2.putText(frame, "****************ALERT!****************", (10, 30),
+                            cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+                cv2.putText(frame, "****************ALERT!****************", (10, 325),
+                            cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+        else:
+            flag = 0
+            alarm_status = False  # Reset alarm status when not drowsy
+    
+    cv2.imshow("Frame", frame)
+    key = cv2.waitKey(1) & 0xFF
+    if key == ord("q"):
+        break
+
+cv2.destroyAllWindows()
+cap.release()
